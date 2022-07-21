0fafbc4d2471b2004b50db68a577dc35f5eb0e1e
erzetid <fahrizalm14@gmail.com>
2022-07-22 02:03:52 +0700

    remove build

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ import dotenv from "dotenv";
 import Server from "./models/Server.js";
 
 dotenv.config();
-const app = new Server(3001);
+
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3000;
+const app = new Server(port || 3000);
 
 app.listen();
 
