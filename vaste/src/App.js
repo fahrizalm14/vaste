d4f23ee34d88faaf69e9a6540402610f5e8fcba9
erzetid <fahrizalm14@gmail.com>
2022-07-22 13:35:57 +0700

    rm cache vaste client

---
+++ vaste/src/App.js
@@ -0,0 +1,28 @@
+import Header from "./Components/Header";
+import MainContent from "./Components/MainContent";
+import Footer from "./Components/Footer";
+import { ButtonTutorial } from "./Components/Button";
+import Favicon from "react-favicon";
+import favicon from "./Images/favicon.ico";
+
+function App() {
+  return (
+    <div
+      style={{
+        marginLeft: "auto",
+        marginRight: "auto",
+        maxWidth: 980,
+        padding: "1rem",
+      }}
+      className="App"
+    >
+      <Favicon url={favicon}></Favicon>
+      <Header></Header>
+      <MainContent></MainContent>
+      <Footer></Footer>
+      <ButtonTutorial></ButtonTutorial>
+    </div>
+  );
+}
+
+export default App;
