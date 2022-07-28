5cdcd54776e68959f1829fc1b6a420071dc05083
erzetid <fahrizalm14@gmail.com>
2022-07-29 01:54:43 +0700

    client initial

--- routes/token.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getController } from "../controller/token.js";
-
-const router = express.Router();
-
-router.get("/", getController);
-
-export default router;
